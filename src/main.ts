a85ac9f52566644e676204f54712a716936c9fc4
pablemus616 <lemuspablo300@gmail.com>
2026-07-10 19:33:15 -0600

    a

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
     transform: true,
     forbidNonWhitelisted: true
   }));
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
