c04c8457e0ba82fc7e68301e764007952fbcc72a
pablemus616 <lemuspablo300@gmail.com>
2026-07-10 19:36:43 -0600

    a

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { ContactRequestDto } from './dto/contact-request.dto';
+import { Public } from '../config/public.decorator';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
+  @Public()
   @Post('contact-request')
   public async createContactRequest(
     @Body() createContactRequest: ContactRequestDto,
